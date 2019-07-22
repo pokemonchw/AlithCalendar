7f15856eb770ce3820ffe78744d52653145940b2
pokemonchw <admin@byayoi.org>
2019-07-22 10:32:32 +0800

    修正npm兼容

--- src/web/ContactMenu.ts
+++ src/web/ContactMenu.ts
@@ -8,9 +8,9 @@ export class ContactMenu extends Menu {
       button: true,
       link: 'https://github.com/pokemonchw/AlithCalendar',
     });
-    this.addItem('作者Mastodon',{
+    this.addItem('作者Mastodon', {
       small: true,
-      button:true,
+      button: true,
       link: 'https://pawoo.net/@nekoharuya',
     });
   }
