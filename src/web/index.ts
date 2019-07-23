e4f419f94374ac66aa543395a035d4619388d7c8
pokemonchw <admin@byayoi.org>
2019-07-23 17:45:01 +0800

    新增神灵和信仰

--- src/web/index.ts
+++ src/web/index.ts
@@ -2,23 +2,9 @@ import { data } from './data';
 import { id } from './DOM';
 import { followQuery } from './followQuery';
 import { MainMenu } from './MainMenu';
-import { animation } from './settings';
 import { updateSelection } from './updateSelection';
 
 const $warning = id('warning');
-
-if ($warning !== null) {
-  $warning.addEventListener('click', () => {
-    $warning.style.opacity = '0';
-    if (animation.getValue()) {
-      $warning.addEventListener('transitionend', () => {
-        $warning.remove();
-      });
-    } else {
-      $warning.remove();
-    }
-  });
-}
 
 const $buildNumber = id('build-number');
 $buildNumber.innerText = `Build ${data.buildNumber}`;
